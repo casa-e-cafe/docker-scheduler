4c13315adb4675e37fbd69dd1f71bf5547d38d73
Kasama <ralegro@bol.com.br>
2017-09-21 16:31:23 -0300

    removed trailing whitespace

--- setup-cron.sh
+++ setup-cron.sh
@@ -6,14 +6,13 @@ TEMPORARY_CRONTAB="crontab.tmp"
 CRON_DIR="/var/spool/cron/crontabs"
 USER="root"
 
-
 touch $TEMPORARY_CRONTAB
 
 for dir in `ls schedules`; do
 	path="$PWD/schedules/$dir"
 
 	if [ -d  $path ]
-	then 
+	then
 		touch TEMPORARY_CRONTAB
 		paste -d' ' <(cat $path/$CRON_FILE) <(echo "$path/$SERVICE_FILE") >> $TEMPORARY_CRONTAB
 	fi
